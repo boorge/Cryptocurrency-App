68bc47c55f4040a24435036c235170cc483b46f2
Kiran Borge <78689154+borgekiran@users.noreply.github.com>
2023-01-02 23:35:53 -0800

    CRYPTOCURRENCY APPLICATION

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="app">
+            <div className="navbar">
+                <Navbar />
+            </div>
+            <div className="main">
+                <Layout>
+                    <div className="routes">
+                        <Switch>
+                            <Route exact path="/">
+                                <Homepage />
+                            </Route>
+                            <Route exact path="/exchanges">
+                                <Exchanges />
+                            </Route>
+                            <Route exact path="/cryptocurrencies">
+                                <Cryptocurrencies />
+                            </Route>
+                            <Route exact path="/crypto/:coinId">
+                                <CryptoDetails />
+                            </Route>
+                            <Route exact path="/news">
+                                <News />
+                            </Route>
+                        </Switch>
+                    </div>
+                </Layout>
+                <div className="footer">
+                    <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+                        Cryptoverse<br />
+                        All rights reserverd
+                    </Typography.Title>
+                    <Space>
+                        <Link to="/">Home</Link>
+                        <Link to="/exchanges">Exchanges</Link>
+                        <Link to="/news">News</Link>
+                    </Space>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default App
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,23 @@
+
+
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+});
+
+export const {useGetCryptosQuery} = cryptoApi;
